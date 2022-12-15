76086686cad3c234d89d83bd77e9a7583b658644
BorisKGB <flot3.sky@yandex.ru>
2022-12-15 22:35:40 +0300

    add task2 solution

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 2 числа в числовом виде.");
+Console.Write("Первое число: ");
+// use Convert.ToInt32() instead int.Parse()
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA > numB)
+{
+    Console.WriteLine($"Первое число {numA} больше чем второе {numB}");
+}
+else if(numA == numB)
+{
+    Console.WriteLine($"Числа {numA} и {numB} равны");
+}
+else
+{
+    Console.WriteLine($"Второе число {numB} больше чем первое {numA}");
+}
