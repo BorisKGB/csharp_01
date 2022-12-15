9ee61bd1aac3fc0e9ad611a05da119ed129f3963
BorisKGB <flot3.sky@yandex.ru>
2022-12-15 22:43:29 +0300

    add task6 solution

---
+++ task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 1 число в числовом виде.");
+Console.Write("Число: ");
+// use Convert.ToInt32() instead int.Parse()
+int number = Convert.ToInt32(Console.ReadLine());
+
+// % -> остаток от деления
+
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечётное");
+}
