2f3ca26009ec5c61f399e72fdffd238346a25b06
BorisKGB <flot3.sky@yandex.ru>
2022-12-15 22:40:13 +0300

    add task4 solution

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите 3 числа в числовом виде.");
+Console.Write("Первое число: ");
+// use Convert.ToInt32() instead int.Parse()
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = numA;
+
+if(numB > maxNum)
+{
+    maxNum = numB;
+}
+if(numC > maxNum)
+{
+    maxNum = numC;
+}
+
+Console.WriteLine($"Из введённых чисел максимальным оказалось {maxNum}");
