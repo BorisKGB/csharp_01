01716ff4777aead131f8917ec304364d72cd1a61
BorisKGB <flot3.sky@yandex.ru>
2022-12-15 22:48:24 +0300

    add task8 solution

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 1 число в числовом виде.");
+Console.Write("Число: ");
+// use Convert.ToInt32() instead int.Parse()
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+Console.WriteLine($"Чётные числа от 1 до {number}:");
+
+while(i <= number)
+{
+    Console.Write($"{i} ");
+    i += 2;
+}
+// В конце выводим пустую строку чтобы сделать перенос строки в терминале
+Console.WriteLine();
